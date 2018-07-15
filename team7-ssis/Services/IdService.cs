4eea4e54c5283a7c90960c8020a3aeedbf76a19e
natebakescakes <natebakescakes@gmail.com>
2018-07-15 18:47:07 +0800

    Complete IdService implementation

--- team7-ssis/Services/IdService.cs
+++ team7-ssis/Services/IdService.cs
@@ -3,71 +3,152 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using team7_ssis.Models;
+using team7_ssis.Repositories;
 
 namespace team7_ssis.Services
 {
     public static class IdService
     {
-        static ApplicationDbContext context = new ApplicationDbContext();
-
-        public static int GetNewCollectionPointId()
+        public static int GetNewCollectionPointId(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            return context.CollectionPoint
+                .OrderByDescending(x => x.CollectionPointId)
+                .FirstOrDefault()
+                .CollectionPointId + 1;
         }
 
-        public static int GetNewDelegationId()
+        public static int GetNewDelegationId(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            return context.Delegation
+                .OrderByDescending(x => x.DelegationId)
+                .FirstOrDefault()
+                .DelegationId + 1;
         }
 
-        public static string GetNewDeliveryOrderNo()
+        public static string GetNewDeliveryOrderNo(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            string prefix = "DO";
+            int serialNo = new DeliveryOrderRepository(context)
+                .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                .Count() >= 0 ?
+                new DeliveryOrderRepository(context)
+                    .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                    .AsEnumerable()
+                    .Select(x => Int32.Parse(x.DeliveryOrderNo.Substring(x.DeliveryOrderNo.Length - 3)))
+                    .OrderByDescending(x => x)
+                    .FirstOrDefault() + 1 : 1;
+
+            return $"{prefix}-{DateTime.Now.Year}{DateTime.Now.Month:00}-{serialNo:000}";
         }
 
-        public static string GetNewDisbursementId()
+        public static string GetNewDisbursementId(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            string prefix = "DSB";
+            int serialNo = new DisbursementRepository(context)
+                .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                .Count() >= 0 ?
+                new DisbursementRepository(context)
+                    .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                    .AsEnumerable()
+                    .Select(x => Int32.Parse(x.DisbursementId.Substring(x.DisbursementId.Length - 3)))
+                    .OrderByDescending(x => x)
+                    .FirstOrDefault() + 1 : 1;
+
+            return $"{prefix}-{DateTime.Now.Year}{DateTime.Now.Month:00}-{serialNo:000}";
         }
 
-        public static string GetNewItemCode()
+        public static string GetNewItemCode(ApplicationDbContext context, ItemCategory itemCategory)
         {
+            // Unsure if can be programmatically generated
             throw new NotImplementedException();
         }
         
-        public static int GetNewItemCategoryId()
+        public static int GetNewItemCategoryId(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            return context.ItemCategory
+                .OrderByDescending(x => x.ItemCategoryId)
+                .FirstOrDefault()
+                .ItemCategoryId + 1;
         }
 
-        public static int GetNewNotificationId()
+        public static int GetNewNotificationId(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            return context.Notification
+                .OrderByDescending(x => x.NotificationId)
+                .FirstOrDefault()
+                .NotificationId + 1;
         } 
 
-        public static string GetNewPurchaseOrderNo()
+        public static string GetNewPurchaseOrderNo(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            string prefix = "PO";
+            int serialNo = new PurchaseOrderRepository(context)
+                .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                .Count() >= 0 ?
+                new PurchaseOrderRepository(context)
+                    .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                    .AsEnumerable()
+                    .Select(x => Int32.Parse(x.PurchaseOrderNo.Substring(x.PurchaseOrderNo.Length - 3)))
+                    .OrderByDescending(x => x)
+                    .FirstOrDefault() + 1 : 1;
+
+            return $"{prefix}-{DateTime.Now.Year}{DateTime.Now.Month:00}-{serialNo:000}";
         }
 
-        public static string GetNewRequisitionId()
+        public static string GetNewRequisitionId(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            string prefix = "REQ";
+            int serialNo = new RequisitionRepository(context)
+                .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                .Count() >= 0 ?
+                new RequisitionRepository(context)
+                    .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                    .AsEnumerable()
+                    .Select(x => Int32.Parse(x.RequisitionId.Substring(x.RequisitionId.Length - 3)))
+                    .OrderByDescending(x => x)
+                    .FirstOrDefault() + 1 : 1;
+
+            return $"{prefix}-{DateTime.Now.Year}{DateTime.Now.Month:00}-{serialNo:000}";
         }
 
-        public static string GetNewRetrievalId()
+        public static string GetNewRetrievalId(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            string prefix = "RET";
+            int serialNo = new RetrievalRepository(context)
+                .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                .Count() >= 0 ?
+                new RetrievalRepository(context)
+                    .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                    .AsEnumerable()
+                    .Select(x => Int32.Parse(x.RetrievalId.Substring(x.RetrievalId.Length - 3)))
+                    .OrderByDescending(x => x)
+                    .FirstOrDefault() + 1 : 1;
+
+            return $"{prefix}-{DateTime.Now.Year}{DateTime.Now.Month:00}-{serialNo:000}";
         }
 
-        public static string GetNewStockAdjustmentId()
+        public static string GetNewStockAdjustmentId(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            string prefix = "ADJ";
+            int serialNo = new StockAdjustmentRepository(context)
+                .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                .Count() >= 0 ?
+                new StockAdjustmentRepository(context)
+                    .FindByCreatedDateTime(DateTime.Now.Date, DateTime.Now.Date.AddDays(1))
+                    .AsEnumerable()
+                    .Select(x => Int32.Parse(x.StockAdjustmentId.Substring(x.StockAdjustmentId.Length - 3)))
+                    .OrderByDescending(x => x)
+                    .FirstOrDefault() + 1 : 1;
+
+            return $"{prefix}-{DateTime.Now.Year}{DateTime.Now.Month:00}-{serialNo:000}";
         }
 
-        public static int GetNewStockMovementId()
+        public static int GetNewStockMovementId(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            return context.StockMovement
+                .OrderByDescending(x => x.StockMovementId)
+                .FirstOrDefault()
+                .StockMovementId + 1;
         }
     }
 }
