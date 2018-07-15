ac4b70d9078d8719758adc3f59f55e67004a8c05
natebakescakes <natebakescakes@gmail.com>
2018-07-15 17:13:02 +0800

    Stub out IdService methods

---
+++ team7-ssis/Services/IdService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using team7_ssis.Models;
+
+namespace team7_ssis.Services
+{
+    public static class IdService
+    {
+        static ApplicationDbContext context = new ApplicationDbContext();
+
+        public static int GetNewCollectionPointId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetNewDelegationId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetNewDeliveryOrderNo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetNewDisbursementId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetNewItemCode()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static int GetNewItemCategoryId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetNewNotificationId()
+        {
+            throw new NotImplementedException();
+        } 
+
+        public static string GetNewPurchaseOrderNo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetNewRequisitionId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetNewRetrievalId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetNewStockAdjustmentId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetNewStockMovementId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
