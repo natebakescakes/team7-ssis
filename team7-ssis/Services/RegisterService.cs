1da636d50db964bd9da0d29ffbebe6def1a0bcc3
jinkanggg <e0282985@u.nus.edu>
2018-07-16 16:49:03 +0800

    method stubs

---
+++ team7-ssis/Services/RegisterService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using team7_ssis.Models;
+
+namespace team7_ssis.Services
+{
+    public class RegisterService
+    {
+        ApplicationDbContext context;
+        public RegisterService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public List<Department> FindAllDepartments()
+        {
+            throw new NotImplementedException();
+        }
+        public ApplicationUser RegisterNewUser()
+        {
+            throw new NotImplementedException();
+        }
+        public ApplicationUser FindUsersByDepartment(Department department)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
