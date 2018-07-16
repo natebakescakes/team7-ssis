0a301e7e53055937471fd9867a742563bfb1c0a3
weinchester <weinchester@gmail.com>
2018-07-16 16:18:39 +0800

    new SupplierService class and method stubs

---
+++ team7-ssis/Services/SupplierService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using team7_ssis.Models;
+using team7_ssis.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace team7_ssis.Services
+{
+    public class SupplierService
+    {
+        public Supplier FindSupplierById(ApplicationDbContext context, string suppliercode)
+        {
+            //mapped from ShowSupplierDetails
+            throw new NotImplementedException();
+        }
+
+        public List<Supplier> FindSupplierBySearchQuery(ApplicationDbContext context, string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(ApplicationDbContext context, string suppliercode)
+        {
+            //mapped from AddNewSupplier, SaveEditDetails, EditPriceList
+            throw new NotImplementedException();
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
