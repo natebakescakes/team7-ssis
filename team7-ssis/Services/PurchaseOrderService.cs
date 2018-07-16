cb04c134f279fc4fbd67a0cdab4585bb40966b22
Raniah Abdul Razak <raniarazzak@gmail.com>
2018-07-16 18:51:20 +0800

    purchase order service

---
+++ team7-ssis/Services/PurchaseOrderService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using team7_ssis.Models;
+
+namespace team7_ssis.Services
+{
+    public class PurchaseOrderService
+    {
+            ApplicationDbContext context;
+
+            public PurchaseOrderService(ApplicationDbContext context)
+            {
+                this.context = context;
+
+            }
+
+            public Item RemoveDraftItemFromPurchaseOrder(string itemCode, PurchaseOrder purchaseOrder)
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<PurchaseOrder> FindAllPurchaseOrders()
+            {
+                throw new NotImplementedException();
+            }
+
+            public PurchaseOrder FindPurchaseOrderById(string purchaseOrderNo)
+            {
+                throw new NotImplementedException();
+            }
+
+            public PurchaseOrderDetail FindPurchaseOrderDetailsById(string purchaseOrderNo)
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<PurchaseOrder> FindPurchaseOrderBySupplier(Supplier supplier)
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<PurchaseOrder> FindPurchaseOrderBySupplier(string supplierCode)
+            {
+                throw new NotImplementedException();
+            }
+
+            public PurchaseOrder FindPurchaseOrderByDeliveryOrder(string deliveryOrderNo)
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<PurchaseOrder> FindPurchaseOrderByStatus(string statusId)
+            {
+                throw new NotImplementedException();
+            }
+
+            public PurchaseOrder Save(PurchaseOrder purchaseOrder)
+            {
+                throw new NotImplementedException();
+            }
+   
+    
+}
