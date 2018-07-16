1829f87815f6de8b9b8c22d6fc38d7020cfedc70
jinkanggg <e0282985@u.nus.edu>
2018-07-16 17:02:31 +0800

    method stub

---
+++ team7-ssis/Services/CollectionPointService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using team7_ssis.Models;
+
+namespace team7_ssis.Services
+{
+    public class CollectionPointService
+    {
+        ApplicationDbContext context;
+        public CollectionPointService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public List<CollectionPoint> FindAllCollectionPoints()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
