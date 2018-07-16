7768a4f8923c5d1e62a8994e6a5d6568ae1e2386
Gabriel Wong <gabrielwong1988@gmail.com>
2018-07-16 17:30:03 +0800

    Created RequisitionService

---
+++ team7-ssis/Services/RequisitionService.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using team7_ssis.Models;
+
+namespace team7_ssis.Services
+{
+    public class RequisitionService : Controller
+    {
+        public ActionResult FindRequisitionsByStatus(List<Status> statusList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult GetRequisitionDetails(string requisitionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ProcessRequisitions(List<Requisition> requestList)
+        {
+            throw new NotImplementedException();
+
+            //
+            // Part 1. Create Retrieval Form w/Details
+            //
+
+            // receive list of requisitions
+            // create HashMap<Item, int> map
+            // get a map of the items you need to retrieve, and how many of them. Implements Retrieval
+
+            // foreach (requisition in requestList):
+            //      foreach (reqDetail in requisition.requisitionDetails):
+            //          if reqDetail.itemCode not in map:
+            //              put(reqDetail.itemCode, reqDetail.quantity)
+            //          else:
+            //              set(reqDetail.itemCode, quantity + reqDetail.quantity)
+
+            // convert the map to a List
+            // return this list for display.
+
+            //
+            // Part 2. Create Disbursement Forms
+            //
+
+            // collect how much each department wanted of each item
+            // - get list of departments in the requestList called deptList
+
+            // foreach (dept in deptList):
+
+            //      create a new Disbursement, d
+            //      d.DepartmentCode = dept.DepartmentCode
+
+            //      foreach (req in reqList):
+            //          foreach (requisitionDetail in req)
+            //              foreach (disbursementDetail in d.DisbursementDetails)
+            //                  if requsitionDetail.itemCode in disbursementDetail.itemCode.values()
+            //                      disbursementDetail.plannedQuantity += requisitionDetail.quantity
+            //                  else:
+            //                      DisbursementDetail db = new DisbursemsentDetail();
+            //                      db.itemCode = requisitionDetail.itemCode;
+            //                      db.plannedQuantity = requisitionDetail.quantity;
+            //                      d.DisbursementDetails.add(db)
+            //
+            //      persist disbursement
+
+        }
+
+        public ActionResult FindRetrievalById(string retrievalId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult UpdateActualQuantities(int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult ContinueToDisbursement()
+        {
+            throw new NotImplementedException();
+        }
+        public ActionResult FindDisbursementsByRetrievalId(string retrievalId)
+        {
+            throw new NotImplementedException();
+        }
+        public ActionResult ViewDisbursementDetails(string disbursementId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult GetDisbursementById(string disbursementId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult ConfirmDelivery(string disbursementId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult CreateNewRequisition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult AddItemPopUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult AddToRequisitions(List<Item> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult CreateNewRequisition(Requisition req)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult ShowRequisition(Requisition req)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult EditRequisition(Requisition req)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult AddItem(List<Item> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult CancelRequisition(Requisition req)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Approve(Requisition req)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult ViewRequisitionDetails(Requisition req)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public ActionResult ApproveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
