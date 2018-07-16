660b12c84ab7dac6b195aef73fa21639f6ad1c28
jinkanggg <e0282985@u.nus.edu>
2018-07-16 16:03:22 +0800

    department service method stubs

---
+++ team7-ssis/Services/DepartmentService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using team7_ssis.Models;
+
+namespace team7_ssis.Services
+{
+    public class DepartmentService
+    {
+        public static void FindDepartmentByUser()
+        {
+            throw new NotImplementedException();
+        }
+        public static void FindAllCollectionPoint()
+        {
+            throw new NotImplementedException();
+        }
+        public static void FindUsersByRole(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+        public static void Submit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
