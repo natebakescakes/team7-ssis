5a19a7573263e9e7f5b2c57d5cb0ff5733da3644
jinkanggg <e0282985@u.nus.edu>
2018-07-17 15:53:59 +0800

    collection point service test

--- team7-ssis.Tests/Services/CollectionPointServiceTests.cs
+++ team7-ssis.Tests/Services/CollectionPointServiceTests.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using team7_ssis.Models;
+using team7_ssis.Services;
 
 namespace team7_ssis.Tests.Services
 {
-    class CollectionPointServiceTests
+    [TestClass]
+    public class CollectionPointServiceTests
     {
+        ApplicationDbContext context;
+        CollectionPointService collectionPointService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // Arrange
+            context = new ApplicationDbContext();
+            collectionPointService = new CollectionPointService(context);
+        }
+        [TestMethod]
+        public void FindAllCollectionPointsTest()
+        {
+            //Arrange
+            int expected = context.CollectionPoint.Count();
+            //Act
+            int result = collectionPointService.FindAllCollectionPoints().Count();
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
     }
 }
