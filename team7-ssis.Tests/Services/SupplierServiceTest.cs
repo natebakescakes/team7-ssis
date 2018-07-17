ed9c38e3c43b2892a01091f560d5a2b314ab0c40
weinchester <weinchester@gmail.com>
2018-07-17 11:42:18 +0800

    implementation of supplier service and unit tests

---
+++ team7-ssis.Tests/Services/SupplierServiceTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using team7_ssis.Models;
+using team7_ssis.Services;
+
+namespace team7_ssis.Tests.Services
+{
+    [TestClass]
+    public class SupplierServiceTest
+    {
+        ApplicationDbContext context;
+        SupplierService supplierService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // Arrange
+            context = new ApplicationDbContext();
+            supplierService = new SupplierService(context);
+        }
+
+        [TestMethod]
+        public void FindSupplierByIdTest()
+        {
+            //Arrange
+            string expected = "BANE";
+
+            //Act
+            var result = supplierService.FindSupplierById(expected);
+
+            //Assert
+            Assert.AreEqual(expected, result.SupplierCode);
+            
+        }
+
+        [TestMethod]
+        public void FindAllSuppliersTest()
+        {
+            //Arrange
+            int expected = context.Supplier.Count();
+            //Act
+            var result = supplierService.FindAllSuppliers().Count();
+            //Assert
+            Assert.AreEqual(expected, result);
+
+        }
+
+        public void SaveTest()
+        {
+            //Arrange
+            Supplier supplier = new Supplier();
+            supplier.SupplierCode = "TEST";
+            //Act
+            var result = supplierService.Save(supplier);
+            //Assert
+            Assert.AreEqual("TEST", result.SupplierCode);
+            Assert.IsNotNull(context.Supplier.Where(x => x.SupplierCode == "TEST").First());
+        }
+
+    }
+}
