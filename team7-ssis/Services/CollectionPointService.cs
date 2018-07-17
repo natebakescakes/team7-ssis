d28fd63c641bd8c1c9fdfda6469782a9af9052dc
jinkanggg <36851310+jinkanggg@users.noreply.github.com>
2018-07-17 16:32:53 +0800

    Collection point service (#20)
    * collectionpointservice methods
    
    * collection point service test

--- team7-ssis/Services/CollectionPointService.cs
+++ team7-ssis/Services/CollectionPointService.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using team7_ssis.Models;
+using team7_ssis.Repositories;
 
 namespace team7_ssis.Services
 {
     public class CollectionPointService
     {
         ApplicationDbContext context;
+        CollectionPointRepository collectionPointRepository;
         public CollectionPointService(ApplicationDbContext context)
         {
             this.context = context;
+            this.collectionPointRepository = new CollectionPointRepository(context);
         }
         public List<CollectionPoint> FindAllCollectionPoints()
         {
-            throw new NotImplementedException();
+            return collectionPointRepository.FindAll().ToList();
         }
     }
 }
