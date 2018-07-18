1406ce29393c3a2e0e77da5ccbbf765c94037c49
weinchester <weinchester@gmail.com>
2018-07-18 17:39:12 +0800

    method stubs for notification service

---
+++ team7-ssis/Services/NotificationService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using team7_ssis.Models;
+using team7_ssis.Repositories;
+
+namespace team7_ssis.Services
+{
+    public class NotificationService
+    {
+        ApplicationDbContext context;
+        NotificationRepository notificationRepository; 
+
+        public NotificationService(ApplicationDbContext context)
+        {
+            this.context = context;
+            notificationRepository = new NotificationRepository(context);
+        }
+
+        public Notification CreateCollectionNotification(Disbursement disbursement, ApplicationUser recipient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Notification CreateStockAdjustmentApprovalNotification(StockAdjustment stockAdjustment, ApplicationUser recpient)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public Notification CreateRequisitionApprovalNotification(Requisition requisition, ApplicationUser recipient)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public List<Notification> FindNotificationsByUser(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Notification> FindNotificationByType(NotificationType type)
+        {
+            throw new NotImplementedException();
+
+        }
+
+    }
+}
