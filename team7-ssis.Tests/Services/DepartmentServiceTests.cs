a71ee22528a99e655b132c4ac2ad32c75846a0be
jinkanggg <e0282985@u.nus.edu>
2018-07-18 11:23:19 +0800

    additional tests

--- team7-ssis.Tests/Services/DepartmentServiceTests.cs
+++ team7-ssis.Tests/Services/DepartmentServiceTests.cs
@@ -35,6 +35,35 @@ namespace team7_ssis.Tests.Services
             //Assert
             Assert.AreEqual(expected, result);
         }
+        [TestMethod]
+        public void FindUsersByDepartmentObjectTest()
+        {
+            //Arrange
+            Department department = context.Department.Where(x => x.DepartmentCode == "COMM").FirstOrDefault();
+            //Act
+            var result = departmentService.FindUsersByDepartment(department);
+            //Assert
+            CollectionAssert.AllItemsAreInstancesOfType(result, typeof(ApplicationUser));
+            result.ForEach(x => Assert.AreEqual("COMM", x.Department));
+        }
+        [TestMethod]
+        public void FindUsersByDepartmentNotNullTest()
+        {
+            //Arrange
+            Department department = new Department();
+            department.DepartmentCode = "ENGL";
+            //Assert
+            CollectionAssert.AllItemsAreNotNull(departmentService.FindUsersByDepartment(department));
+        }
+        [TestMethod]
+        public void FindUsersByDepartmentUniqueTest()
+        {
+            //Arrange
+            Department department = new Department();
+            department.DepartmentCode = "ENGL";
+            //Assert
+            CollectionAssert.AllItemsAreUnique(departmentService.FindUsersByDepartment(department));
+        }
         //[TestMethod]
         //public void FindDepartmentByUserTest()
         //{
