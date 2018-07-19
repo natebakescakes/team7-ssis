1b155c0c7ba9ec70da27b083efae00d644238f53
weinchester <weinchester@gmail.com>
2018-07-19 14:02:46 +0800

    initial commit for supplier controller and view

---
+++ team7-ssis/Controllers/SupplierController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace team7_ssis.Controllers
+{
+    public class SupplierController : Controller
+    {
+        // GET: Supplier
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
