29c767ac02162653f581afe74ed8d66baa87f132
weinchester <weinchester@gmail.com>
2018-07-19 23:16:10 +0800

    API controller for suppliers list

---
+++ team7-ssis/Controllers/SupplierAPIController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using team7_ssis.Models;
+using team7_ssis.Services;
+using team7_ssis.ViewModels;
+
+namespace team7_ssis.Controllers
+{
+    public class SupplierAPIController : ApiController
+    {
+        static ApplicationDbContext context = new ApplicationDbContext();
+        SupplierService supplierService = new SupplierService(context);
+
+        [Route("api/suppliers/all")]
+       [HttpGet]
+        public IEnumerable<SupplierViewModel> Suppliers()
+        {
+            List<Supplier> list = supplierService.FindAllSuppliers();
+            List<SupplierViewModel> suppliers = new List<SupplierViewModel>();
+
+            foreach (Supplier s in list)
+            {
+                suppliers.Add(new SupplierViewModel
+                {
+                    SupplierCode = s.SupplierCode,
+                    Address = s.Address,
+                    PhoneNumber = s.PhoneNumber,
+                    ContactName = s.ContactName,
+                    FaxNumber = s.FaxNumber,
+                    Name = s.Name
+
+                });
+            }
+
+            return suppliers;
+        }
+        
+    }
+}
