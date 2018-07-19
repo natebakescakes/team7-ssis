37dfc6c90718641b588755c5a10a083439cbe18e
aridoaridokadotoji <30995417+aridoaridokadotoji@users.noreply.github.com>
2018-07-19 14:09:40 +0800

    Item service (#43)
    * Writing method name in ItemService
    
    * After merging again
    
    * After fixing comments by Nathan
    
    * Adding ItemPriceService, ItemCategoryService, StockMovementService
    
    * change string type of int
    
    * Implemented ItemService methods
    
    * Changed DeleteItem()
    
    * Add ItemPriceService, ItemCategoryService Without Testing
    
    * After running all tests
    
    * test1
    
    * test2
    
    * Adding 2 test functions to ItemServiceTest
    
    * Finished ItemService Testing
    
    * Update team7-ssis.csproj
    
    * Finish ItemPriceService Testing
    Finish ItemService Testing
    Finish Inventory Functions
    
    * changing connection string
    
    * After fixing dependency bug
    
    * After fixing bug dependency of ItemPriceTest
    
    * Finish ItemCategoryService Testing
    
    * Adding Inventory Controller and ManageInventory View as a testing
    
    * Testing controller and Adding FindItemQuantityLessThanReorderLevel()

---
+++ team7-ssis/Controllers/InventoryAPIController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using team7_ssis.Services;
+using team7_ssis.Models;
+
+namespace team7_ssis.Controllers
+{
+    public class InventoryAPIController : ApiController
+    {
+        public static ApplicationDbContext context = new ApplicationDbContext();
+        ItemService itemService = new ItemService(context);
+        public IEnumerable<Item> GetAllItem()
+        {
+            var k = itemService.FindAllItems();
+            return k;
+        }
+    }
+}
