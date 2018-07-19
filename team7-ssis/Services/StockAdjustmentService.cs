78383bee4933469f003528d591624f7dc78d9579
hejiashuang2018 <hejiashuang2018@outlook.com>
2018-07-19 10:06:40 +0800

    ApproveStockAdjustmentService and Test part

--- team7-ssis/Services/StockAdjustmentService.cs
+++ team7-ssis/Services/StockAdjustmentService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using team7_ssis.Models;
 using team7_ssis.Repositories;
+using team7_ssis.Services;
 
 namespace team7_ssis.Tests.Services
 {
@@ -13,12 +14,15 @@ namespace team7_ssis.Tests.Services
         StockAdjustmentRepository stockAdjustmentRepository;
         StockAdjustmentDetailRepository stockAdjustmentDetailRepository;
         StatusRepository statusRepository;
+        ItemService itemService;
+
         public StockAdjustmentService(ApplicationDbContext context)
         {
             this.context = context;
             this.statusRepository = new StatusRepository(context);
             this.stockAdjustmentRepository = new StockAdjustmentRepository(context);
             this.stockAdjustmentDetailRepository = new StockAdjustmentDetailRepository(context);
+            this.itemService = new ItemService(context);
         }
 
         //create new StockAdjustment with status: draft
@@ -136,6 +140,7 @@ namespace team7_ssis.Tests.Services
                 foreach (StockAdjustmentDetail sd in stockadjustment.StockAdjustmentDetails)
                 {
                     // update each Item inventory = sd.AfterQuantity;
+                    itemService.UpdateQuantity(sd.Item, sd.AfterQuantity);                   
                 }
 
             }
