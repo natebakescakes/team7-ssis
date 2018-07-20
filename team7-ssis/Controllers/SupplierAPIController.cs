552e81ef7cdebf6f11c6940a66648e93df3a6e1e
Jawen Voon Kakiong <36097628+weinchester@users.noreply.github.com>
2018-07-20 12:21:58 +0800

    Manage supplier (#47)
    * initial commit for supplier controller and view
    
    * API controller for suppliers list
    
    * data table working
    
    * data table working with layout
    
    * latest commit
    
    * changed datatable datasource to API
    
    * Update SupplierAPIController to use lambda
    
    * Remove LoadData method from SupplierController.cs

---
+++ team7-ssis/Controllers/SupplierAPIController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using team7_ssis.Models;
+using team7_ssis.Services;
+using team7_ssis.ViewModels;
+
+namespace team7_ssis.Controllers
+{
+    public class SupplierAPIController : ApiController
+    {
+        static ApplicationDbContext context = new ApplicationDbContext();
+        SupplierService supplierService = new SupplierService(context);
+
+        [Route("api/supplier/all")]
+        [HttpGet]
+        public List<SupplierViewModel> Suppliers()
+        {
+            return supplierService.FindAllSuppliers().Select(supplier => new SupplierViewModel()
+            {
+                SupplierCode = supplier.SupplierCode,
+                Address = supplier.Address,
+                PhoneNumber = supplier.PhoneNumber,
+                ContactName = supplier.ContactName,
+                FaxNumber = supplier.FaxNumber,
+                Name = supplier.Name
+            }).ToList();
+        }
+    }
+}
