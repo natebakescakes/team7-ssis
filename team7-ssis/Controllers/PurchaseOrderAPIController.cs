e8112ab887a9c018cb14ca0931101f43c67c86a2
Raniah Abdul Razak <raniarazzak@gmail.com>
2018-07-20 13:26:38 +0800

    Added JS and view file for purchase order

--- team7-ssis/Controllers/PurchaseOrderAPIController.cs
+++ team7-ssis/Controllers/PurchaseOrderAPIController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 using team7_ssis.Models;
 using team7_ssis.ViewModels;
 using team7_ssis.Services;
-using System.Web.Mvc;
+
 
 namespace team7_ssis.Controllers
 {
@@ -16,14 +16,29 @@ namespace team7_ssis.Controllers
         public static ApplicationDbContext context = new ApplicationDbContext();
         PurchaseOrderService purchaseOrderService = new PurchaseOrderService(context);
 
-        public IEnumerable<PurchaseOrderViewModel> LoadData()
+        [Route("api/purchaseOrders/all")]
+        [HttpGet]
+        public IEnumerable<PurchaseOrderViewModel> PurchaseOrders()
         {
-           context.Configuration.LazyLoadingEnabled = false; // if your table is relational, contain foreign key
+           //context.Configuration.LazyLoadingEnabled = false; // if your table is relational, contain foreign key
             List<PurchaseOrder> poList = purchaseOrderService.FindAllPurchaseOrders();
 
-            
-            
+            List<PurchaseOrderViewModel> data = new List<PurchaseOrderViewModel>();
 
+            foreach(PurchaseOrder p in poList)
+            {
+                data.Add(new PurchaseOrderViewModel
+                {
+                    PNo = p.PurchaseOrderNo,
+                    SupplierName = p.Supplier.Name,
+                    CreatedDate = p.CreatedDateTime,
+                    Status = p.Status.Name
+                
+                });
+ 
+            }
+
+            return data;
         }
        
 
--- team7-ssis/Scripts/purchaseOrderJS.js
+++ team7-ssis/Scripts/purchaseOrderJS.js
@@ -2,8 +2,8 @@
 $(document).ready(function(){
 	
 
-        var u = window.contextRoot + '/PurchaseOrderApi/loaddata';
-		var $table=$('#myTable');
+        
+		var $table=$('#poTable');
 	
 		
 		//var simple_checkbox=function(data,type,full, meta){
@@ -15,145 +15,33 @@ $(document).ready(function(){
 		var datatbl = $table.DataTable(
 				{
 					ajax: {
-                        url: 'api/PurchaseOrderApi/loaddata',
-                        type: 'get',
-                        dataType: 'json'
-					},
-					columns: [
+                        sAjaxSource: 'api/purchaseOrders/all',
+                        sAjaxDataProp: ''
+                },
+                select: 'single',
+                columns: [
+                        //{
+                        //defaultContent: '<input type="checkbox"  class="checkbox" />'
+                        //},
 						{
-							data: 'userid'
+							data: 'PNo'
 						},
 						{
-							data: 'username'
+							data: 'SupplierName'
 						},
 						{
-							data: 'firstName'
+							data: 'CreatedDate'
 						},
 						{
-							data: 'lastName'
-						},
-						{
-							data: 'email'
-						},
-						{
-							data: 'address'
-						},
-						{
-							data: 'phone'
-						},
-						{
-							data: 'dob'
-						},
-						{
-							data: 'enabled',
-							render: simple_checkbox	
-						},
-						{
-							data: 'admin',
-							render: simple_checkbox
-						},
-						{
-							defaultContent: "<button class='td-button btn-edit btn btn-table btn-primary'>Edit</button>"
+							data: 'Status'
 						}
-						
 					]
 					
 				});
 		
 		
 		
-
-	     //$('#myTable tbody').on('click', '.btn-edit', function (e) {
-	     //    var data = datatbl.row( $(this).parents('tr') ).data();
-	    	// var counter = 0;
-	     //    $($(this).parents('tr')).find("td").each(function(){
-      //  		 counter++;
-	     //   	 if(counter < 3){
-	     //   		 return;
-	     //   	 }
-
-	     //   	 if ($(this).children().hasClass("checkbox"))
-	     //   	 {
-	     //   		( $(this).find("input")).prop("disabled",false);
-	        		 
-	     //   	 }
-	     //   	 if (!$(this).children().hasClass("td-button"))
-	     //   	    {
-	     //   	        var text = $(this).text();
-	     //   	        $(this).html ('<input type="text" size=10 value="' +  text + '">');
-	     //   	    } 
-	     //   	 if ($(this).children().hasClass("btn-edit"))
-	     //   		 {
-	     //   	       $(this).html ('<button class="td-button btn-save btn btn-table btn-primary">Submit</button>');
-	     //   		 }
-	     //   	   })
-	      
-	     // } );
-	     
-	     //$('#myTable tbody').on('click', '.btn-save', function (e) {
-    		// var parenttr = $(this).parents('tr');
-	    	// var counter = 0;
-	    	// $($(this).parents('tr')).find("td").each(function(){
-    		
-      //  		 counter++;
-	     //   	 if(counter < 3){
-	     //   		 return;
-	     //   	 }
-	        	 
-	     //   	 if ($(this).children().hasClass("checkbox"))
-	     //   	 {
-		    //    		( $(this).find("input")).prop("disabled",true);
-		        		
-		    //    		if ( ($(this).find("input")).prop('checked')==true)
-		    //    		{
-				  //      	 var cell = datatbl.cell( $(this) );
-				        	 
-				  //      	 cell.data(1).draw();
-		   	//        	}
-		        		
-		   	//        	 else
-		   	//        	 {
-			   //	        	 var cell = datatbl.cell( $(this) );
-			   //	        	 cell.data(0).draw();
-		   	//        	 }
-	        		
-	     //   	 }
-	        	 
-	        	 
-	     //   	 if (!$(this).children().hasClass("td-button"))
-	     //   	    {
-	     //   		 	var text = $(this).find("input").val();
-	     //   		 	$(this).text(text);
-	     //   		 	var cell = datatbl.cell( $(this) );
-	     //   		 	cell.data( text ).draw();
-	        	   
-	     //   	    } 
-	
-		    	 
-	     //   	 if ($(this).children().hasClass("btn-save"))
-	     //   		 {
-	     //   	        $(this).html ('<button class="td-button btn-edit btn btn-table btn-primary">Edit</button>');
-	     //   		 } 
-	        	 
-	    	// }) ;
-	        
-	    	 
-	     //    var memberdata = datatbl.row( parenttr ).data();
-	       
-
-	         
-	     //    $.ajax({
-	     //        url: window.contextRoot + "/admin/members/update",
-	     //        type: "POST",
-	     //        data: JSON.stringify(memberdata),
-	     //        contentType: "application/json",
-	     //        cache: true,
-	     //        success: function (result) {
-	            	
-	     //        }
-	     //      });
-	      	    
-	     // } );    
+  
 	    
 
 });
