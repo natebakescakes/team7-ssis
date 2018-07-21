92183fba118b903d307e94c66f4187ff62400a7e
Nathan Khoo <natebakescakes@gmail.com>
2018-07-21 17:53:51 +0800

    Complete user management related features (#53)
    * Clean up overall code
    
    * Allow registration with custom fields
    
    * Update to latest stable owin
    
    * Add related test cases
    
    * Fix FindUsersByDepartmentObjectTest assert statement
    
    * Implement FindSupervisorByDepartment and related unit tests
    
    * Create View Profile page
    
    * Implement profile editing
    
    * Fix to show current value in dropdown when editing profile
    
    * Created and unit tested UserApiController
    
    * Add more UserApi actions
    
    * Implement javascript dropdown population of supervisors for registration and profile pages
    
    * Fix merge conflicts

--- team7-ssis/Models/ManageViewModels.cs
+++ team7-ssis/Models/ManageViewModels.cs
@@ -15,48 +15,6 @@ namespace team7_ssis.Models
         public bool BrowserRemembered { get; set; }
     }
 
-    public class ViewProfileViewModel
-    {
-        public string Title { get; set; }
-        [Display(Name = "First Name")]
-        public string FirstName { get; set; }
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Department { get; set; }
-        public string Supervisor { get; set; }
-
-    }
-
-    public class EditProfileViewModel
-    {
-        public string Title { get; set; }
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-        [Required]
-        public string TitleId { get; set; }
-        [Display(Name = "Title")]
-        public IEnumerable<SelectListItem> Titles { get; set; }
-        [Required]
-        [Display(Name = "First Name")]
-        public string FirstName { get; set; }
-        [Required]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-        [Required]
-        public string DepartmentCode { get; set; }
-        public string Department { get; set; }
-        [Display(Name = "Department")]
-        public IEnumerable<SelectListItem> Departments { get; set; }
-        [Required]
-        public string SupervisorEmail { get; set; }
-        public string Supervisor { get; set; }
-        [Display(Name = "Supervisor")]
-        public IEnumerable<SelectListItem> Supervisors { get; set; }
-    }
-
     public class ManageLoginsViewModel
     {
         public IList<UserLoginInfo> CurrentLogins { get; set; }
