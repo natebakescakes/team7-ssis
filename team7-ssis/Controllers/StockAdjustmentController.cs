6d8e83bd9c65a534bcf08a45bfa4b47ed666f744
hejiashuang2018 <hejiashuang2018@outlook.com>
2018-07-22 11:36:41 +0800

    show dropdownlist

--- team7-ssis/Controllers/StockAdjustmentController.cs
+++ team7-ssis/Controllers/StockAdjustmentController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using team7_ssis.Models;
+using team7_ssis.Repositories;
+using team7_ssis.Services;
 using team7_ssis.Tests.Services;
 
 namespace team7_ssis.Controllers
@@ -12,7 +14,8 @@ namespace team7_ssis.Controllers
     {
         public static ApplicationDbContext context = new ApplicationDbContext();
         StockAdjustmentService stockAdjustmentService = new StockAdjustmentService(context);
-
+        UserService userService = new UserService(context);
+        UserRepository userRepository = new UserRepository(context);
 
         // GET: StockAdjustment
         public ActionResult Index()
@@ -21,7 +24,8 @@ namespace team7_ssis.Controllers
           }
         public ActionResult Home()
         {
-
+            ApplicationUser user = userRepository.FindById("166ad865 - 7f9a - 4673 - 8838 - 68e51e0fcd3e");
+            ViewBag.supervisors = userService.listSupervisorsByUser(user);
             return View();
         }
 
