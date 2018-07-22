20b4adb14400263872bac02d18779a7ac2eb6cb9
PLValli <E0302271@u.nus.edu>
2018-07-22 08:03:30 +0800

    To rebase

---
+++ team7-ssis/Scripts/OutstandingItems.js
@@ -0,0 +1,25 @@
+﻿    $(document).ready(function () {
+
+            var oTable = $('#myOutstandingTable').DataTable({
+
+        "ajax": {
+
+            "url": "outstandingitems",
+
+                    "type": "get",
+
+                    "datatype": "json"
+
+                },
+
+                "columns": [
+
+                    {"data": "Item Code", "autoWidth": true},
+
+                    {"data": "Description", "autoWidth": true },
+
+                    {"data": "Outstanding Qty", "autoWidth": true },
+
+                ]
+            });
+
