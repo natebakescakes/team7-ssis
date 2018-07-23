9d7467472ff36e2fb41901d871756ed1e17884cb
jiashuang2018 <36067683+jiashuang2018@users.noreply.github.com>
2018-07-23 10:16:55 +0800

    StockAdjustment DataTable (#50)
    * Add ManageStockAdjustmentService
    (change AddItem to FIndAllItems
    change AddItems to SelectItems
    Change DeleteItem to DeleteCurrentSelected)
    
    *  update StockAdjustmentService
    
    * Add more method in StockAdjustmentService(cover ApproveStockAdjustment and ManageStockAdjustment)
    
    * StockAdjustmentService.cs
    
    * implemente StockAdjustmentService
    
    * add comment and change method name
    
    * Delete ManageStockAdjustmentService.cs
    
    * add stock_adjustment_service_test
    
    * Add unittest for stockadjustment
    
    * update Test and Service
    
    * update
    FindAllStockAdjustmentTest
    
    * update DeleteItemFromDraftStockAdjustmentTest
    
    * delete redundant file
    
    * change deletedraft... to canceldraft...
    
    * add exception handle
    
    * update item inventory after approve stockadjustment
    
    * ApproveStockAdjustmentService and Test part
    
    * Add Controller(1)
    
    * change config to cloud
    
    * add apicontroller
    
    *  update Stockmovement afetr approve
    
    * TestCleanUp
    
    * StockAdjustmentapicontroller
    
    * add some method
    
    * Home page of stockadjustment
    
    * show dropdownlist
    
    * add Knockot.js
    
    * Update csproj file

---
+++ team7-ssis/Controllers/StockAdjustmentAPIController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using team7_ssis.Models;
+using team7_ssis.Tests.Services;
+using team7_ssis.ViewModels;
+using team7_ssis.Repositories;
+
+namespace team7_ssis.Controllers
+{
+    
+    public class StockAdjustmentAPIController : ApiController
+    {
+        static ApplicationDbContext context = new ApplicationDbContext();
+        StockAdjustmentService stockAdjustmentService = new StockAdjustmentService(context);
+        StockAdjustmentRepository stockAdjustmentRepository = new StockAdjustmentRepository(context); 
+
+
+
+    
+        [Route("api/stockadjustment/all")]
+        [HttpGet]
+        public IEnumerable<StockAdjustmentViewModel> stockadjustments()
+        {
+            List<StockAdjustment> list = stockAdjustmentService.FindAllStockAdjustment();
+            List<StockAdjustmentViewModel > sadj= new List<StockAdjustmentViewModel>();
+
+
+            foreach (StockAdjustment s in list)
+            {
+                sadj.Add(new StockAdjustmentViewModel
+                {
+                    StockAdjustmentId = s.StockAdjustmentId,
+                    CreatedBy = (s.CreatedBy == null) ? "" : s.CreatedBy.FirstName,
+                    ApprovedBySupervisor = (s.ApprovedBySupervisor == null) ? "" : s.ApprovedBySupervisor.FirstName,
+                    CreatedDateTime = s.CreatedDateTime,
+                    StatusName = (s.Status == null) ? "" : s.Status.Name,
+                    Link = "/StockAdjustment/"+s.StockAdjustmentId
+                });
+
+            }
+
+            return sadj;
+        }
+
+
+
+        [Route("api/supervisor/all")]
+        [HttpGet]
+
+        public IEnumerable<ApplicationUser>  AllSupervisors()
+        {
+            return null;
+        }
+        [Route("api/manager/all")]
+        [HttpGet]
+        public IEnumerable<ApplicationUser> AllManagers()
+        {
+            return null;
+        }
+
+    }
+}
