14877f63f243e3b0df8a9f344886ab4eba72a87d
hejiashuang2018 <hejiashuang2018@outlook.com>
2018-07-23 12:43:44 +0800

    partail view of index

---
+++ team7-ssis/Scripts/Custom/StockAdjustment.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    $('#mySATable').DataTable({
+
+        sAjaxSource: "/api/stockadjustment/all",
+        sAjaxDataProp: "",
+        columns: [
+            { "data": "StockAdjustmentId", "autoWidth": true },
+            { "data": "CreatedBy", "autoWidth": true },
+            { "data": "ApprovedBySupervisor", "autoWidth": true },
+            { "data": "CreatedDateTime", "autoWidth": true },
+            { "data": "StatusName", "autoWidth": true },
+
+        ]
+    });
+    //Apply Customer Search on jquery Datatables here
+//    var oTable = $('#mySATable').dataTable();
+//    $('#btnSearch').click(function () {
+//        oTable.columns[4].search($('#addStatus').val().trim());
+//    oTable.draw();
+//});
+});
+
+
+
+
+
+
+
+
+
+
