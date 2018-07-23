cf33b279d6def80c4c9cbaf8213f80b0e6f0d531
Gabriel Wong <gabrielwong1988@gmail.com>
2018-07-23 17:58:56 +0800

    Partially implemented /Disbursement/DisbursementDetails

---
+++ team7-ssis/Controllers/DisbursementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace team7_ssis.Controllers
+{
+    public class DisbursementController : Controller
+    {
+        // GET: Disbursement/DisbursementDetails
+        public ActionResult DisbursementDetails()
+        {
+            return View();
+        }
+    }
+}
