ff23fc581db2ab6ede7be46e178b83968d123554
Gabriel Wong <gabrielwong1988@gmail.com>
2018-07-24 17:29:05 +0800

    Implemented /Requisition/RequisitionDetail

--- team7-ssis/Controllers/RequisitionController.cs
+++ team7-ssis/Controllers/RequisitionController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using team7_ssis.Models;
+using team7_ssis.Repositories;
 using team7_ssis.Services;
 using team7_ssis.ViewModels;
 
@@ -13,6 +14,7 @@ namespace team7_ssis.Controllers
     {
         static ApplicationDbContext context = new ApplicationDbContext();
         RequisitionService requisitionService = new RequisitionService(context);
+        RequisitionRepository requisitionRepository = new RequisitionRepository(context);
         RetrievalService retrievalService = new RetrievalService(context);
         ItemService itemService = new ItemService(context);
         
@@ -23,9 +25,27 @@ namespace team7_ssis.Controllers
         }
 
         // GET: /Requisiton/RequisitionDetails
-        public ActionResult RequisitionDetails()
+        public ActionResult RequisitionDetails(string reqId)
         {
-            return View();
+            Requisition r = requisitionRepository.FindById(reqId);
+            if (reqId == null)
+            {
+                return new HttpStatusCodeResult(400);
+            }
+
+            RequisitionDetailViewModel viewModel = new RequisitionDetailViewModel();
+            
+            viewModel.RequisitionID = r.RequisitionId;
+            viewModel.Department = r.Department == null ? "" : r.Department.Name;
+            viewModel.CollectionPoint = r.CollectionPoint == null ? "" : r.CollectionPoint.Name;
+            viewModel.CreatedBy = r.CreatedBy == null ? "" : String.Format("{0} {1}", r.CreatedBy.FirstName, r.CreatedBy.LastName);
+            viewModel.CreatedTime = String.Format("{0} {1}", r.CreatedDateTime.ToShortDateString(), r.CreatedDateTime.ToShortTimeString());
+            viewModel.UpdatedBy = r.UpdatedBy == null ? "" : String.Format("{0} {1}", r.UpdatedBy.FirstName, r.UpdatedBy.LastName);
+            viewModel.UpdatedTime = r.UpdatedDateTime == null ? "" : String.Format("{0} {1}", r.UpdatedDateTime.Value.ToShortDateString(), r.UpdatedDateTime.Value.ToShortTimeString());
+            viewModel.ApprovedBy = r.ApprovedBy == null ? "" : String.Format("{0} {1}", r.ApprovedBy.FirstName, r.ApprovedBy.LastName);
+            viewModel.ApprovedTime = r.ApprovedDateTime == null ? "" : String.Format("{0} {1}", r.ApprovedDateTime.Value.ToShortDateString(), r.ApprovedDateTime.Value.ToShortTimeString());
+
+            return View(viewModel);
         }
         // GET: /Requisiton/StationeryRetrieval
         [Route("/Requisition/StationeryRetrieval")]
--- team7-ssis/ViewModels/RequisitionViewModels.cs
+++ team7-ssis/ViewModels/RequisitionViewModels.cs
@@ -20,4 +20,33 @@ namespace team7_ssis.ViewModels
         public string Description { get; set; }
         public int QtyOrdered { get; set; }
     }
+    public class RequisitionDetailViewModel
+    {
+        public string RequisitionID { get; set; }
+        public string Department { get; set; }
+        public string CollectionPoint { get; set; }
+        public string CreatedBy { get; set; }
+        public string CreatedTime { get; set; }
+        public string UpdatedBy { get; set; }
+        public string UpdatedTime { get; set; }
+        public string ApprovedBy { get; set; }
+        public string ApprovedTime { get; set; }
+    }
+    public class StationeryDisbursementViewModel
+    {
+        public string DisbursementID { get; set; }
+        public string Department { get; set; }
+        public string CollectionPoint { get; set; }
+        public string DisbursedBy { get; set; }
+        public string Status { get; set; }
+    }
+    public class ManageRequisitionsViewModel
+    {
+        public string Requisition { get; set; }
+        public string ItemCode { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string Status { get; set; }
+    }
+
 }
