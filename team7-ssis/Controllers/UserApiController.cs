fbf35e7d5a73dfd855546180b238e6a7e25b385c
Nathan Khoo <natebakescakes@gmail.com>
2018-07-24 15:22:45 +0800

    Add Role Api for Android (#72)
    * Implement RESTful token authentication
    
    * Confirmed token-based authentication via JSON
    
    * Update authorize attribute and url for NotificationApiController
    
    * Add FindRoleByEmail service method and related repositories + unit tests
    
    * Implement GetRoles API call and unit tests
    
    * Update UserApi to JSON post action

--- team7-ssis/Controllers/UserApiController.cs
+++ team7-ssis/Controllers/UserApiController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
 using team7_ssis.Models;
 using team7_ssis.Services;
 
@@ -44,6 +48,20 @@ namespace team7_ssis.Controllers
                 Email = supervisor.Email,
                 Name = $"{supervisor.FirstName} {supervisor.LastName}"
             }));
+        }
+
+        [HttpPost]
+        [Route("api/users/roles")]
+        public IHttpActionResult GetRoles([FromBody] EmailNameViewModel model)
+        {
+            var roles = new UserService(context).FindRolesByEmail(model.Email);
+
+            if (roles.Count == 0) return NotFound();
+
+            return Ok(new RoleViewModel()
+            {
+                Roles = roles
+            });
         }
     }
 }
