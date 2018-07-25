40747ce3885d8747586f81b320a2e8c1517a319b
Gabriel Wong <gabrielwong1988@gmail.com>
2018-07-25 10:17:11 +0800

    Implement Create/Manage/Process Requisition Screens (#74)
    * Created RequisitionService
    
    * Create RequisitionServiceTests
    
    * Streamlined RequisitionService
    
    * Updated RequisitionServiceTests
    
    * Changed RequisitionService to be class instead of controller
    
    * FIxed namespace for RequisitionServiceTests
    
    * Implemented CreateDisbursementByDepartment
    
    * Implemented createDisbursementsByDepartmentTest
    
    * Fixed StationeryStoreInit
    
    * Fixed CreateDisbursementForEachDepartment
    
    * Fixed CreateDisbursementForEachDepartmentTest
    
    * Fixed ProcessRequisitions
    
    * Added ProcessRequisitionsTest
    
    * Added AddDisbursementDetailsForEachDepartmentTest
    
    * Fixed AddDisbursementDetailsForEachDisbursement
    
    * Fixed ProcessRequisitions
    
    * Added FindRequisitionByStatus and FindRequisitionDetails to RequisitionRepository
    
    * Implemented RequisitionService methods
    
    * FindRequisitionsByStatus
    * GetRequisitionDetails
    
    * Added FindRequisitionByStatus
    
    * Multiple changes:
    * Moved TestInitialize into ClassInitialize
    * Moved TestCleanup into ClassCleanup
    * Added FindRequisitionsByStatusTest
    
    * Fixed CreateDisbursementForEachDepartment
    
    * Fixed ProcessRequisitions
    
    * Fixed ProcessRequisitions_CreatesRetrieval
    
    * Fixed FindRequisitionsByStatusTest
    
    * Fixed FindRequisitionsByStatusTest
    
    * Added ProcessRequisitionsTest_AddQuantity
    
    * Fixed test methods
    * DisbursementServiceTest.SaveTest()
    * RequisitionServiceTests namespace fix
    
    * Implemented /Requisition
    * Created RequisitionAPIController
    * Created ManageRequisitionsViewModel
    * Created Views\Requisition
    * Small typo on RequisitionServiceTests
    
    * Make DataTables compatible with Bootstrap 4
    
    * Created Requisition/StationeryRetrieval
    
    * Created StationeryDisbursement
    
    * Small typos in StationeryRetrieval
    
    * Added DataTable Select extension CSS
    
    * Small typo
    
    * /Requisition redirects to StationeryRetrieval upon successful AJAX call
    
    * Processing selected Requisition Ids in /Requisition
    
    * Implemented /StationeryRetrieval
    
    * Partially implemented /Requisition/StationeryDisbursement
    
    * Partially implemented /Requisition/RetrievalDetail
    
    * Partially implemented /Disbursement/DisbursementDetails
    
    * Partially implemented /Requisition/CreateRequisition
    
    * Implemented Disbursement/DisbursementDetails
    
    * Add RedirectPost.js
    
    * Implemented StationeryRetrieval
    
    * Implemented StationeryRetrivalTest
    
    * Implemented /Requisition/StationeryDisbursement
    
    * Implemented /Requisition/RequisitionDetail
    
    * Implemented RequisitonDetailsTests
    
    * Implemented Requisition/RetrievalDetails
    
    * Implemented RetrievalDetailsTest
    
    * Created Requisition/CreateRequisition
    
    * Created Requisition/CreateRequisition

---
+++ team7-ssis/Controllers/DisbursementAPIController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using team7_ssis.Models;
+using team7_ssis.Services;
+using team7_ssis.ViewModels;
+
+namespace team7_ssis.Controllers
+{
+    public class DisbursementAPIController : ApiController
+    {
+        public static ApplicationDbContext context = new ApplicationDbContext();
+        DisbursementService disbursementService = new DisbursementService(context);
+        
+        /// <summary>
+        /// Retrieves all Disbursement Details for the Disbursement
+        /// </summary>
+        [Route("api/disbursement/{did}")]
+        [HttpGet]
+        public IEnumerable<DisbursementFormTableViewModel> FindDisbursementDetailsByDisbursement (string did)
+        {
+            Disbursement d = disbursementService.FindDisbursementById(did);
+            List<DisbursementFormTableViewModel> viewModel = new List<DisbursementFormTableViewModel>();
+            foreach (DisbursementDetail dd in d.DisbursementDetails)
+            {
+                viewModel.Add(new DisbursementFormTableViewModel
+                {
+                    ItemCode = dd.ItemCode,
+                    Description = dd.Item.Description,
+                    Qty = dd.ActualQuantity
+                });
+            }
+            return viewModel;
+        }
+    }
+}
--- team7-ssis/Controllers/RequisitionController.cs
+++ team7-ssis/Controllers/RequisitionController.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using team7_ssis.Models;
+using team7_ssis.Repositories;
 using team7_ssis.Services;
+using team7_ssis.ViewModels;
 
 namespace team7_ssis.Controllers
 {
@@ -12,55 +16,109 @@ namespace team7_ssis.Controllers
     {
         static ApplicationDbContext context = new ApplicationDbContext();
         RequisitionService requisitionService = new RequisitionService(context);
+        RequisitionRepository requisitionRepository = new RequisitionRepository(context);
         RetrievalService retrievalService = new RetrievalService(context);
         ItemService itemService = new ItemService(context);
-        
+        CollectionPointService collectionPointService = new CollectionPointService(context);
+        DepartmentService departmentService = new DepartmentService(context);
+        UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
         // GET: /Requisition
-        public ActionResult Index()
+        public ActionResult ManageRequisitions()
         {
             return View();
         }
 
         // GET: /Requisiton/RequisitionDetails
-        public ActionResult RequisitionDetails()
+        public ActionResult RequisitionDetails(string reqId)
         {
-            return View();
+            Requisition r = requisitionRepository.FindById(reqId);
+            if (reqId == null)
+            {
+                return new HttpStatusCodeResult(400);
+            }
+
+            RequisitionDetailViewModel viewModel = new RequisitionDetailViewModel();
+            
+            viewModel.RequisitionID = r.RequisitionId;
+            viewModel.Department = r.Department == null ? "" : r.Department.Name;
+            viewModel.CollectionPoint = r.CollectionPoint == null ? "" : r.CollectionPoint.Name;
+            viewModel.CreatedBy = r.CreatedBy == null ? "" : String.Format("{0} {1}", r.CreatedBy.FirstName, r.CreatedBy.LastName);
+            viewModel.CreatedTime = String.Format("{0} {1}", r.CreatedDateTime.ToShortDateString(), r.CreatedDateTime.ToShortTimeString());
+            viewModel.UpdatedBy = r.UpdatedBy == null ? "" : String.Format("{0} {1}", r.UpdatedBy.FirstName, r.UpdatedBy.LastName);
+            viewModel.UpdatedTime = r.UpdatedDateTime == null ? "" : String.Format("{0} {1}", r.UpdatedDateTime.Value.ToShortDateString(), r.UpdatedDateTime.Value.ToShortTimeString());
+            viewModel.ApprovedBy = r.ApprovedBy == null ? "" : String.Format("{0} {1}", r.ApprovedBy.FirstName, r.ApprovedBy.LastName);
+            viewModel.ApprovedTime = r.ApprovedDateTime == null ? "" : String.Format("{0} {1}", r.ApprovedDateTime.Value.ToShortDateString(), r.ApprovedDateTime.Value.ToShortTimeString());
+
+            return View(viewModel);
         }
         // GET: /Requisiton/StationeryRetrieval
-        [Route("/Requisition/StationeryRetrieval/{id}")]
-        [HttpGet]
-        public ActionResult StationeryRetrieval(string id)
+        [Route("/Requisition/StationeryRetrieval")]
+        public ActionResult StationeryRetrieval(string rid)
         {
-            // TODO: Remove hardcoded values
-            id = "RET-201807-001";
-            Retrieval r = retrievalService.FindRetrievalById(id);
-            ViewBag.Retrieval = r;
-            return View();
+            if (rid == null)
+            {
+                return new HttpStatusCodeResult(400);
+            }
+
+            Retrieval r = retrievalService.FindRetrievalById(rid);
+            StationeryRetrievalViewModel viewModel = new StationeryRetrievalViewModel();
+            viewModel.RetrievalID = r.RetrievalId;
+
+            try { viewModel.CreatedBy = String.Format("{0} {1}", r.CreatedBy.FirstName, r.CreatedBy.LastName); }
+            catch { viewModel.CreatedBy = ""; }
+            viewModel.CreatedOn = String.Format("{0} {1}", r.CreatedDateTime.ToShortDateString(), r.CreatedDateTime.ToShortTimeString());
+            try
+            {
+                viewModel.UpdatedBy = String.Format("{0} {1}", r.UpdatedBy.FirstName, r.UpdatedBy.LastName);
+            } catch
+            {
+                viewModel.UpdatedBy = "";
+            }
+            try
+            {
+                viewModel.UpdatedOn = String.Format("{0} {1}", r.UpdatedDateTime.Value.ToShortDateString(), r.UpdatedDateTime.Value.ToShortTimeString());
+            } catch
+            {
+                viewModel.UpdatedOn = "";
+            }
+
+            return View(viewModel);
         }
         // GET: /Requisiton/StationeryDisbursement
-        public ActionResult StationeryDisbursement()
+        public ActionResult StationeryDisbursement(string rid)
         {
-            // TODO: Remove hardcorded values
-            ViewBag.Retrieval = "RET-201807-001";
+            ViewBag.RetrievalID = rid;
             return View();
         }
-        [HttpGet]
         public ActionResult RetrievalDetails(string retId, string itemId)
         {
-            // TODO: Remove hardcoded values
-            retId = "RET-201807-001";
-            itemId = "C001";
+            if (retId == null || itemId == null)
+            {
+                return new HttpStatusCodeResult(400);
+            }
 
+            RetrievalDetailsViewModel viewModel = new RetrievalDetailsViewModel();
             Item i = itemService.FindItemByItemCode(itemId);
 
-            ViewBag.RetrievalId = retId;
-            ViewBag.Item = i;
+            viewModel.ProductID = i.ItemCode;
+            viewModel.Name = i.Name;
+            viewModel.Bin = i.Bin;
 
-            return View();
+            return View(viewModel);
         }
         public ActionResult CreateRequisition()
         {
-            return View();
+            CreateRequisitionsViewModel viewModel = new CreateRequisitionsViewModel();
+            try
+            {
+                viewModel.Representative = departmentService.FindDepartmentByUser(userManager.FindById(User.Identity.GetUserId())).ContactName;
+            } catch
+            {
+                viewModel.Representative = "";
+            }
+            viewModel.SelectCollectionPointList = collectionPointService.FindAllCollectionPoints();
+            return View(viewModel);
         }
     }
 }
