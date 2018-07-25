ade0f1e791194f3382a434037946c85e8ffc124e
Nathan Khoo <natebakescakes@gmail.com>
2018-07-25 12:34:39 +0800

    Create DisbursementAPI for mobile (#75)
    * Implement GetAllDisbursements API for mobile view
    
    * Controllers to use instanced dbContext

--- team7-ssis/Controllers/DisbursementAPIController.cs
+++ team7-ssis/Controllers/DisbursementAPIController.cs
@@ -12,9 +12,15 @@ namespace team7_ssis.Controllers
 {
     public class DisbursementAPIController : ApiController
     {
-        public static ApplicationDbContext context = new ApplicationDbContext();
-        DisbursementService disbursementService = new DisbursementService(context);
-        
+        ApplicationDbContext context;
+        DisbursementService disbursementService;
+
+        public DisbursementAPIController()
+        {
+            context = new ApplicationDbContext();
+            disbursementService = new DisbursementService(context);
+        }
+
         /// <summary>
         /// Retrieves all Disbursement Details for the Disbursement
         /// </summary>
@@ -34,6 +40,32 @@ namespace team7_ssis.Controllers
                 });
             }
             return viewModel;
+        }
+
+        /// <summary>
+        /// Get all disbursements for mobile view
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/disbursement")]
+        public IHttpActionResult GetAllDisbursements()
+        {
+            var disbursements = disbursementService.FindAllDisbursements();
+
+            if (disbursements.Count == 0) return NotFound();
+
+            return Ok(disbursements.Select(disbursement => new DisbursementMobileViewModel()
+            {
+                DisbursementId = disbursement.DisbursementId,
+                Department = disbursement.Department != null ? disbursement.Department.Name : "",
+                CollectionPoint = disbursement.Retrieval.Requisitions.Where(r => r.Department.Name == disbursement.Department.Name).FirstOrDefault().CollectionPoint.Name,
+                Status = disbursement.Status != null ? disbursement.Status.Name : "",
+                DisbursementDetails = disbursement.DisbursementDetails.Select(d => new DisbursementFormTableViewModel()
+                {
+                    ItemCode = d.ItemCode,
+                    Description = d.Item.Description,
+                    Qty = d.ActualQuantity
+                }).ToList()
+            }));
         }
     }
 }
