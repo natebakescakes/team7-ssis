2fb1afd56742eeb313440f44f4b901394d2f3803
PLValli <37648995+PLValli@users.noreply.github.com>
2018-08-01 13:40:41 +0800

    Implement Receive Goods functionality (#148)
    * Added DeliveryOrderServices
    
    * Made changes as per comments in github
    
    * No changes
    
    * Added methods to services
    
    * Changed service name
    
    * Rename deliveryorderservice.cs to DeliveryOrderService.cs
    
    * Update team7-ssis.csproj
    
    * Method for saving delivery order is included in the service
    
    * Delete deliveryorderservice.cs
    
    * remove deliveryorderservice.cs
    
    * add deliveryorderservice.cs
    
    * deliveryorder and deliverorderservice with test methods working
    
    * save inventory,save stockmovement tested
    
    * Pull latest changes from master
    
    * DeliveryOrderAPI created
    
    * Receive goods view completed
    
    * Stock Movement linked to Delivery order details
    
    * Saved repositories after roollbaclk
    
    * Bootstrap Merge
    
    * Fixed Stock Movement inserting null values
    
    * Commit Delivery Order Test Changes
    
    * Clean up code
    
    * Revert ItemPriceServiceTest changes
    
    * To rebase
    
    * Outstanding Items
    
    * Receivegoods
    
    * Implement receivegoods screen(Incomplete)
    
    * Implementing outstanding po
    
    * Implemented Index page
    
    * Implemented outstanding items PO
    
    * Merge
    
    * implemented confirm button
    
    * Implemented receivegoods for a purchaseorder
    
    * Implemented cancel button
    
    * Implemented filters
    
    * Implemented View Related Delivery ORder
    
    * Implementing save
    
    * Fixed project file
    
    * included a method to calculate remaining quantity
    
    * Saving Deliveryorder implemented
    
    * File upload, change cancel status implemented
    
    * test methods implemented
    
    * commit
    
    * Implemented test services

--- team7-ssis/Controllers/DeliveryOrderController.cs
+++ team7-ssis/Controllers/DeliveryOrderController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using team7_ssis.Services;
 using team7_ssis.Models;
+using team7_ssis.ViewModels;
+using Microsoft.AspNet.Identity;
+using System.IO;
 
 namespace team7_ssis.Controllers
 {
@@ -13,23 +16,172 @@ namespace team7_ssis.Controllers
         private ApplicationDbContext context;
         private DeliveryOrderService deliveryOrderService;
         private PurchaseOrderService purchaseOrderService;
+        private PurchaseOrderService purchaseOrderDetailService;
+        private UserService userService;
+        private StatusService statusService;
+        private SupplierService supplierService;
+        private ItemService itemService;
+
 
         public DeliveryOrderController()
         {
             context = new ApplicationDbContext();
             deliveryOrderService = new DeliveryOrderService(context);
             purchaseOrderService = new PurchaseOrderService(context);
+            purchaseOrderDetailService = new PurchaseOrderService(context);
+            userService = new UserService(context);
+            statusService = new StatusService(context);
+            supplierService = new SupplierService(context);
+            itemService = new ItemService(context);
         }
 
         // GET: DeliveryOrder
         public ActionResult Index()
         {
-            return View("ReceiveGoods");
+            return View();
         }
 
-        public ActionResult OutstandingItems()
+        [HttpGet]
+        public ActionResult ReceiveGoods()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult ReceivedGoodsPurchaseOrderView(string pon)
+        {
+            PurchaseOrderViewModel POVM = new PurchaseOrderViewModel();
+
+            POVM.PurchaseOrderNo = pon;
+
+            return View(POVM);
+        }
+
+        [HttpPost]
+        public ActionResult ReceiveGoodsView(string ponum)
+        {
+            PurchaseOrderViewModel POVM = new PurchaseOrderViewModel();
+            PurchaseOrder purchaseOrder = purchaseOrderDetailService.FindPurchaseOrderById(ponum);
+
+            POVM.PurchaseOrderNo = purchaseOrder.PurchaseOrderNo;
+
+            POVM.SupplierName = purchaseOrder.Supplier.Name;
+
+            POVM.CreatedDate = purchaseOrder.CreatedDateTime.ToShortDateString();
+
+            POVM.Status = purchaseOrder.Status.Name;
+
+            return View(POVM);
+        }
+
+        [HttpPost]
+
+        public ActionResult ChangeStatus(DeliveryOrderDetailsViewModel model)
+        {
+            PurchaseOrderDetail purchaseOrderDetail = purchaseOrderDetailService.FindPurchaseOrderDetailbyIdItem(model.PurchaseOrderNo, model.ItemCode);
+            purchaseOrderDetail.Status = statusService.FindStatusByStatusId(2);
+            purchaseOrderService.SavePurchaseOrderDetail(purchaseOrderDetail);
+            return new JsonResult { Data = new { status = "Saved" } };
+        }
+
+
+        [HttpPost]
+
+        public ActionResult Save(List<DeliveryOrderDetailsViewModel> deliveryOrderDetailViewList)
+        {
+            DeliveryOrder deliveryOrder = new DeliveryOrder();
+
+            deliveryOrder.DeliveryOrderDetails = new List<DeliveryOrderDetail>();
+
+            deliveryOrder.PurchaseOrder = purchaseOrderService.FindPurchaseOrderById(deliveryOrderDetailViewList[0].PurchaseOrderNo);
+
+
+            deliveryOrder.DeliveryOrderNo = IdService.GetNewDeliveryOrderNo(context);
+            deliveryOrder.CreatedDateTime = DateTime.Now;
+
+
+            deliveryOrder.CreatedBy = userService.FindUserByEmail(System.Web.HttpContext.Current.User.Identity.GetUserName());
+
+            deliveryOrder.Supplier = supplierService.FindSupplierById(deliveryOrder.PurchaseOrder.SupplierCode);
+
+            deliveryOrder.DeliveryOrderFileName = Path.GetFileName(deliveryOrderDetailViewList[0].DeliveryOrderFileName);
+
+            deliveryOrder.InvoiceFileName = Path.GetFileName(deliveryOrderDetailViewList[0].InvoiceFileName);
+
+
+            deliveryOrder.Status = statusService.FindStatusByStatusId(1);
+
+            deliveryOrderService.Save(deliveryOrder);
+
+            deliveryOrder.PurchaseOrder.DeliveryOrders.Add(deliveryOrder);
+
+            deliveryOrderService.Save(deliveryOrder);
+            
+
+
+            foreach (DeliveryOrderDetailsViewModel dovm in deliveryOrderDetailViewList)
+            {
+                PurchaseOrderDetail purchaseOrderDetail = purchaseOrderService.FindPurchaseOrderDetailbyIdItem(deliveryOrder.PurchaseOrder.PurchaseOrderNo, dovm.ItemCode);
+
+                if (dovm.CheckboxStatus == dovm.ItemCode)
+                {
+                    purchaseOrderDetail.Status = statusService.FindStatusByStatusId(3);
+                    break;
+                }
+
+               if (dovm.ReceivedQty != 0) 
+                { 
+
+                    DeliveryOrderDetail deliveryOrderDetail = new DeliveryOrderDetail();
+
+                    deliveryOrderDetail.DeliveryOrderNo = deliveryOrder.DeliveryOrderNo;
+
+                    deliveryOrderDetail.Item = itemService.FindItemByItemCode(dovm.ItemCode);
+
+                    deliveryOrderDetail.ItemCode = dovm.ItemCode;
+
+                    deliveryOrderDetail.PlanQuantity = dovm.QtyOrdered;
+
+                    deliveryOrderDetail.ActualQuantity = dovm.ReceivedQty;
+
+                    deliveryOrderDetail.Status = statusService.FindStatusByStatusId(1);
+
+                    deliveryOrderDetail.UpdatedBy = userService.FindUserByEmail(System.Web.HttpContext.Current.User.Identity.GetUserName());
+
+                    deliveryOrderDetail.UpdatedDateTime = DateTime.Now;
+
+                    deliveryOrder.DeliveryOrderDetails.Add(deliveryOrderDetail);
+                }
+
+            }
+
+            deliveryOrderService.CheckSave(deliveryOrder);
+
+            return new JsonResult { Data = new { status = "Saved" } };
+        }
+
+        [HttpPost]
+        public ActionResult DOConfirmationView(string dno)
+        {
+            DeliveryOrderViewModel DOVM = new DeliveryOrderViewModel();
+            DeliveryOrder deliveryOrder = deliveryOrderService.FindDeliveryOrderById(dno);
+            DOVM.DeliveryOrderNo = dno;
+
+            DOVM.PurchaseOrderNo = deliveryOrder.PurchaseOrder.PurchaseOrderNo;
+
+            DOVM.SupplierName=deliveryOrder.Supplier.Name;
+
+            DOVM.Status = deliveryOrder.PurchaseOrder.Status.Name;
+
+            DOVM.DeliveryOrderFileName = deliveryOrder.DeliveryOrderFileName;
+
+            DOVM.InvoiceFileName = deliveryOrder.InvoiceFileName;
+
+            DOVM.CreatedBy = deliveryOrder.CreatedBy.FirstName + deliveryOrder.CreatedBy.LastName;
+
+            DOVM.CreatedDate = deliveryOrder.CreatedDateTime;
+
+            return View(DOVM);
         }
     }
 }
