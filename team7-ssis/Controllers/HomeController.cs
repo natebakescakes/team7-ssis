0117d65c196ae2a057337b8f8c3b0711186b37ec
natebakescakes <natebakescakes@gmail.com>
2018-08-02 16:52:29 +0800

    Redirect all users except employees to Manage Requisitions page

--- team7-ssis/Controllers/HomeController.cs
+++ team7-ssis/Controllers/HomeController.cs
@@ -5,13 +5,33 @@ using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using team7_ssis.Models;
+using team7_ssis.Services;
 using team7_ssis.ViewModels;
 
 namespace team7_ssis.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController()
+        {
+            Context = new ApplicationDbContext();
+        }
+
+        public ApplicationDbContext Context { get; set; }
+
         public ActionResult Index()
+        {
+            // If not Employee role
+            if (!User.IsInRole("Employee"))
+                return RedirectToAction("ManageRequisitions", "Requisition");
+            // If Department Representative
+            else if (new UserService(Context).FindUserByEmail(User.Identity.Name).Department.Representative.Email == User.Identity.Name)
+                return RedirectToAction("ManageRequisitions", "Requisition");
+
+            return RedirectToAction("Unauthorized");
+        }
+
+        public ActionResult Unauthorized()
         {
             return View();
         }
