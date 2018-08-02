39b5fce0c065dd52ab95bab377ee54c7df375824
weinchester <weinchester@gmail.com>
2018-08-02 11:38:11 +0800

    fixed context on notification api controller

--- team7-ssis/Controllers/NotificationApiController.cs
+++ team7-ssis/Controllers/NotificationApiController.cs
@@ -19,7 +19,7 @@ namespace team7_ssis.Controllers
     [RoutePrefix("api/Notification")]
     public class NotificationApiController : ApiController
     {
-        private ApplicationDbContext context;
+        public ApplicationDbContext context { get; set; }
         private static string pixel= "c4rWWOQR6Ms:APA91bEaZlfPSZ6_P17Y-ZK5w1T0cIW6knoA8Vf8heX9NnNptP1sEIKVWJlXrEHb2_qiso1JQeQ02QUg78bhKwGXi2P5lip1UK0JZ39T5j8w7xTX8e6QkA8uH-i0Q3GL1GYQ3zYQw9BkQMG1iFhvyg7SpK0W-nJt1w";
         private static string nexus = "cbyMssJA0mE:APA91bF8DdZVBtnag_4UrjdthkrU_t489E4nVH_TAoEpmhIu023u-dhTWoWdh1_FSuWceWYhrHxv336G-WYLoz8gs-4IF-NuVEZXVqMts-HnSfLeHQU67_0gZbTDaSzyGNFibh0ksqJvzl_iHZKmdiT8Mf9RrDMhgw";
         private static string ServerKey = "AAAASdSX054:APA91bEPhId59S4Qtl00O3llHaXrPay5RjXDiMnip9ofs1IbIssamoFS20PrSOyC47wIFetCJyWwbxy0SzTIm0hvOh_titJu5OfNvXUpEMFZL2g4vVRY5n1B_bOBrgaZ5tUWQ-jQFJz4Xh7O3gw9seWV86p7Oon5Eg";
@@ -54,7 +54,7 @@ namespace team7_ssis.Controllers
             }));
         }
 
-        [Route("api/notification/send/{id}")]
+        [Route("send/{id}")]
         [HttpGet]
         public IHttpActionResult SendNotification(string id)
         {
@@ -71,11 +71,9 @@ namespace team7_ssis.Controllers
             httpWebRequest.Headers.Add("Authorization:key=" + ServerKey);
             httpWebRequest.Method = "POST";
 
-            string AppKey = nexus;
-
-            ////check who is the user
-            //if (CurrentUserName == "CommerceHead@email.com") AppKey = pixel;
-            //else AppKey = pixel;
+            string AppKey;
+            if (notification.CreatedFor == null) AppKey = pixel;
+            else AppKey = nexus;
 
             FirebaseObject obj = new FirebaseObject()
             {
