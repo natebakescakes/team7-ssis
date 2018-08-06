d875212d005cdcadb0e4a8def66978821cd17d36
raniarazak <raniarazzak@gmail.com>
2018-08-06 09:17:20 +0800

    Purchase order service (#323)
    * minor styling chnages, print pdf of disbursement form changed to viewasPDF
    
    * minor styling changes

--- team7-ssis/Scripts/Custom/DeliveryOrder.js
+++ team7-ssis/Scripts/Custom/DeliveryOrder.js
@@ -80,6 +80,10 @@ $(document).ready(function () {
     //Receive goods- View DeliveryOrders - purchaseordernumber
 
     var rgTable = $('#myRGTable').DataTable({
+        language: {
+            search: "_INPUT_",
+            searchPlaceholder: "Search..." 
+        },
         ajax: {
             url: "/api/receivegoods/" + pon,
             dataSrc: ""
@@ -237,7 +241,8 @@ $(document).ready(function () {
                     }
                 }
             ],
-        select: "single"
+        select: "single",
+        dom:"tp"
     });
    
     // change the value of the cell in the datatable with an input field
